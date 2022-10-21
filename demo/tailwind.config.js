896bacf802f175b103039e68d2b958a42538a775
yenbekbay <ayan.yenb@gmail.com>
2022-10-21 19:13:17 +0600

    chore(demo): fix bad tailwind config

--- demo/tailwind.config.js
+++ demo/tailwind.config.js
@@ -1,12 +1,10 @@
-const colors = require('tailwindcss/colors')
-
 /**
  * @type {import('tailwindcss').Config}
  */
 module.exports = {
   content: [
     './{game,pages}/**/*.{ts,tsx}',
-    '../packages/react-visual-novel/{commands,components,contexts}/**/*.{ts,tsx}',
+    './node_modules/react-visual-novel/dist/index.js',
   ],
   theme: {
     extend: {
@@ -24,7 +22,17 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+    // @ts-ignore
+    require('tailwindcss-scrims')({
+      colors: {
+        default: ['rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0, 0)'],
+        light: ['rgba(255, 255, 255, 0.5)', 'rgba(255, 255, 255, 0)'],
+      },
+    }),
+  ],
   daisyui: {
     styled: true,
     logs: false,
